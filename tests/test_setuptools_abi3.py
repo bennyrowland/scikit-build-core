9237050cdd94ca1c7407d6381aa3952cd11a9ff9
Ben Rowland <ben.rowland@hallmarq.net>
2023-02-20 11:26:01 +0000

    chore: experiment with abi3 test failure

--- tests/test_setuptools_abi3.py
+++ tests/test_setuptools_abi3.py
@@ -1,6 +1,5 @@
 import shutil
 import sys
-import zipfile
 from pathlib import Path
 
 import pytest
@@ -37,21 +36,21 @@ def test_abi3_wheel(tmp_path, monkeypatch, virtualenv):
     assert "-cp37-abi3-" in out
     print("printing hello")
     assert virtualenv.execute("print('hello')") == "hello"
-    print("finished printing hello")
-    if sys.version_info >= (3, 8):
-        with wheel.open("rb") as f:
-            p = zipfile.Path(f)
-            file_names = {p.name for p in p.iterdir()}
-
-        so_file = (
-            "abi3_example.abi3.dll"
-            if sys.platform.startswith("cygwin")
-            else "abi3_example.abi3.so"
-        )
-        assert so_file in file_names
-    print("installing wheel")
-    virtualenv.install(wheel)
-    print("running the abi3 code")
-    output = virtualenv.execute("import abi3_example; print(abi3_example.square(2))")
-    print("asserting output")
-    assert output == "4.0"
+    # print("finished printing hello")
+    # if sys.version_info >= (3, 8):
+    #     with wheel.open("rb") as f:
+    #         p = zipfile.Path(f)
+    #         file_names = {p.name for p in p.iterdir()}
+    #
+    #     so_file = (
+    #         "abi3_example.abi3.dll"
+    #         if sys.platform.startswith("cygwin")
+    #         else "abi3_example.abi3.so"
+    #     )
+    #     assert so_file in file_names
+    # print("installing wheel")
+    # virtualenv.install(wheel)
+    # print("running the abi3 code")
+    # output = virtualenv.execute("import abi3_example; print(abi3_example.square(2))")
+    # print("asserting output")
+    # assert output == "4.0"
